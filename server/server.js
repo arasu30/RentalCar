736eb1ba373b6c7cce72baf5fb42dd743fda95cb
Ilavarasu Thevar <arasuthevar01@gmail.com>
2025-08-19 05:41:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-
+import userRouter from "./routes/userRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 // Initialize Express App
 const app = express()
@@ -15,7 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=> res.send("Server is running"))
+app.use('/api/user', userRouter)
 
+app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
